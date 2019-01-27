c066b7e24e3478cf2ad1229fa3cdd7293e51a2d4
qcymkxyc <qcymkxyc@163.com>
2019-01-27 16:31:32 +0800

    完成第3题第二遍

--- main1/__init__.py
+++ main1/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 #!/usr/bin/env python
 # _*_coding:utf-8_*_
 
@@ -8,16 +7,4 @@
     @File: __init__.py.py
     @Software: PyCharm
     
-    
-=======
-#!usr/bin/env python 
-# -*- coding:utf-8 _*-  
-""" 
- @author:qcymkxyc
- @email:qcymkxyc@163.com
- @software: PyCharm
- @file: __init__.py.py
- @time: 2019/1/21 11:38
- 
->>>>>>> Stashed changes
 """
